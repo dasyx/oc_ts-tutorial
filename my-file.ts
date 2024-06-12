90791dbf8aa5dc9cd3498aa0430bf3c129ba6855
unknown <seanpnn00@gmail.com>
2024-06-12 11:49:48 +0200

    first commit

---
+++ my-file.ts
@@ -0,0 +1,11 @@
+const age: number = 10;
+
+// type npx tsc my-file.ts in order to compile the file
+// this will create a my-file.js file
+// we can add the --watch flag to watch for changes
+// npx tsc my-file.ts --watch
+// we can also add the --outFile flag to compile multiple files into one
+// we can also add the --removeComments flag to remove comments from the output
+// npx tsc my-file.ts --outFile my-file.js --removeComments
+// we can also add es6 as the target to compile to es6
+// npx tsc my-file.ts --target es6
